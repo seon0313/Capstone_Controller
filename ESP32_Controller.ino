794d676b66b8e7b7ba14735d648a11a94e0d7f5a
seon0313 <seon06.dev@gmail.com>
2024-06-16 22:55:32 +0900

    Update Code

--- ESP32_Controller.ino
+++ ESP32_Controller.ino
@@ -1,6 +1,11 @@
 #include <Adafruit_MPU6050.h>
 #include <Arduino.h>
+#include <Stepper.h>
 Adafruit_MPU6050 mpu;
+
+const int stepsPerRevolution = 200;
+
+Stepper left(stepsPerRevolution, 15, 2, 0, 4);
 
 void initMpu6050(){
   if (!mpu.begin()){
@@ -13,11 +18,12 @@ void initMpu6050(){
 void setup() {
   // put your setup code here, to run once:
   Serial.begin(115200);
+  left.setSpeed(300);
   initMpu6050();
 }
 sensors_event_t a, g, temp;
 float accX, accY, accZ;
-
+int d= 1;
 void loop() {
   // put your main code here, to run repeatedly:
   mpu.getEvent(&a, &g, &temp);
@@ -27,5 +33,7 @@ void loop() {
   Serial.println(String(accX));
   Serial.println(String(accY));
   Serial.println(String(accZ));
+  left.step(stepsPerRevolution*d);
+  d*=-1;
   delay(100);
 }
