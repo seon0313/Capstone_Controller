77e670e6dbde71fb72139d584ab4cc36b9fe079a
seon0313 <seon06.dev@gmail.com>
2024-07-02 22:23:15 +0900

    Add P.I.D Control

--- ESP32_Controller.ino
+++ ESP32_Controller.ino
@@ -1,27 +1,49 @@
 #include <Adafruit_MPU6050.h>
 #include <Arduino.h>
-#include <Stepper.h>
+#include <AccelStepper.h>
 #include <ESP32Servo.h>
+
 Adafruit_MPU6050 mpu;
 
-const int stepsPerRevolution = 200;
+float acceleration = 200;
+float maxSpeed = 99999999;
 
-Stepper left(stepsPerRevolution, 4,0,2,15);//15, 2, 0, 4);
+int left_step = 33;
+int left_dir = 25;
+int right_step = 26;
+int right_dir = 27;
+
+AccelStepper left(AccelStepper::DRIVER, left_step, left_dir);
+AccelStepper right(AccelStepper::DRIVER, right_step, right_dir);
 
 int servoLeftPin = 14;
 int servoRightPin = 12;
 
-float servoLeft_Offset = -28;
-float servoRight_Offset = -80;
+float servoLeft_Offset = 0;
+float servoRight_Offset = 0;
 
 Servo servoLeft;
 Servo servoRight;
 
-float angle = 90;
+float angle = 270;
 
 void writeServo(float angle){
   servoLeft.write(angle+servoLeft_Offset);
   servoRight.write(270-angle+servoRight_Offset);
+}
+
+void writeStep(float var){
+  left.setSpeed(var);
+  right.setSpeed(-var);
+  left.runSpeed();
+  right.runSpeed();
+}
+
+void initStep(){
+  left.setAcceleration(acceleration);
+  right.setAcceleration(acceleration);
+  left.setMaxSpeed(maxSpeed);
+  right.setMaxSpeed(maxSpeed);
 }
 
 void initMpu6050(){
@@ -52,10 +74,20 @@ void setup() {
   left.setSpeed(300);
   initMpu6050();
   initServo();
+  initStep();
 }
 sensors_event_t a, g, temp;
 float accX, accY, accZ;
 int d= 1;
+
+long unsigned old_time = millis();
+
+float Kp = 100; // PID제어 P 값
+float Ki = 5; // PID제어 I 값
+float Kd = 10; // PID제어 D 값
+
+float pre_error;
+
 void loop() {
   // put your main code here, to run repeatedly:
   mpu.getEvent(&a, &g, &temp);
@@ -65,6 +97,15 @@ void loop() {
   Serial.println(String(accX));
   Serial.println(String(accY));
   Serial.println(String(accZ));
-  left.step(stepsPerRevolution*d);
-  delay(100);
+
+  float error = 0-accY;
+  int P = Kp * error;
+  float delta_time = millis()-old_time;
+  int I = Ki * error * delta_time;
+  int D = Kd * (error - pre_error) / delta_time;
+  int pid = P + I + D; 
+  writeStep(pid);
+  pre_error = error;
+
+  writeServo(angle);
 }
